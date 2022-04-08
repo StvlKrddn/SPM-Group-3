7b1d90344f70f1405aed2a1527fb8b25d1c0f94e
StvlKrddn <august.lundquist01@gmail.com>
2022-04-08 21:48:00 +0200

    Började implementera Input System

--- SPM Grupp3/Assets/Scripts/Augusts Testscripts/TankController.cs
+++ SPM Grupp3/Assets/Scripts/Augusts Testscripts/TankController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TankController : MonoBehaviour
 {
@@ -10,33 +11,58 @@ public class TankController : MonoBehaviour
     [SerializeField] private float movementSpeed = 5;
     [SerializeField] private float turnSpeed = 2;
 
-    private float movementInput;
-    private float rotationInput;
+    private Rigidbody rb;
+    private PlayerInput playerInput;
+
+    // Caching input actions
+    private InputAction moveAction;
+    private InputAction dashAction;
+    private InputAction shootAction;
+
+    private Vector3 inputVector;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        InitializeInputSystem();
+    }
+
+    void InitializeInputSystem()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions["Move"];
+        dashAction = playerInput.actions["Dash"];
+        shootAction = playerInput.actions["Shoot"];
+    }
 
     // Update is called once per frame
     void Update()
     {
         GatherInput();
+    }
+
+    void FixedUpdate()
+    {
         Turn();
         Move();
     }
 
     void GatherInput()
     {
-        // For moving back and forth
-        movementInput = Input.GetAxisRaw("Vertical");
-
-        // For rotating tank
-        rotationInput = Input.GetAxisRaw("Horizontal");
+        inputVector = new Vector3(Input.GetAxisRaw("Vertical"), Input.GetAxisRaw("Horizontal"), 0);
     }
 
     void Move()
     {
-        transform.Translate(0, 0, movementInput * movementSpeed * Time.deltaTime);
+        Vector3 movement = transform.forward * inputVector.x * movementSpeed * Time.deltaTime;
+        rb.MovePosition(transform.position + movement);
     }
 
     void Turn()
     {
-        transform.Rotate(0, rotationInput * turnSpeed, 0);
+        Vector3 rotationVector = new Vector3(0, inputVector.y * turnSpeed * Time.deltaTime * 100f, 0);
+        Quaternion rotation = Quaternion.Euler(rotationVector);
+        rb.MoveRotation(rb.rotation * rotation);
     }
 }
