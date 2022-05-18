8fc0659e4d84aceca780894fdfd56474ee5ef32a
Markus Koffman <markus.koffman@ownit.nu>
2022-05-18 13:49:03 +0200

    Revert "Update EnemyPortal.cs"
    This reverts commit 877b08ba7b0dc5dea86c5a701ae53f7bc9e60991.

--- SPM Grupp3/Assets/Scripts/Enemies/EnemyTypes/EnemyPortal.cs
+++ SPM Grupp3/Assets/Scripts/Enemies/EnemyTypes/EnemyPortal.cs
@@ -6,15 +6,15 @@ public class EnemyPortal : EnemyController
 {
 	[SerializeField] private float timer;
 	private bool opened = false;
-	private BoxCollider boxCollider;
+	private BoxCollider collider;
 	private int randomWaypoint;
 
 	protected override void Awake()
 	{
 		base.Awake();
 
-		boxCollider = GetComponent<BoxCollider>();
-		boxCollider.enabled = false;
+		collider = GetComponent<BoxCollider>();
+		collider.enabled = false;
 		RandomizeTargets();
 		StartCoroutine(OpenPortal());
 		//Start Particle
@@ -41,7 +41,7 @@ public class EnemyPortal : EnemyController
 	{
 		yield return new WaitForSeconds(timer);
 		opened = true;
-		boxCollider.enabled = true;
+		collider.enabled = true;
 		//Stop particle
 		//Stop invis
 		yield return null;
