b654ae33e1b1ca6be456fef7b978fb99f6894f82
MarcusHedenberg <82215349+MarcusHedenberg@users.noreply.github.com>
2022-05-18 13:22:57 +0200

    t

--- SPM Grupp3/Assets/Scripts/Player/Builder/BuilderController.cs
+++ SPM Grupp3/Assets/Scripts/Player/Builder/BuilderController.cs
@@ -71,7 +71,7 @@ public class BuilderController : MonoBehaviour
 
     void InitializeCursor()
     {
-        playerCursor = Instantiate(cursorPrefab, transform.parent.Find("PlayerCanvas"));
+        playerCursor = Instantiate(cursorPrefab, transform.parent.Find("PlayerUI"));
         SetCursorColor(playerCursor);
         playerCursor.name = "Player " + (playerInput.playerIndex + 1) + " cursor";
         cursorTransform = playerCursor.GetComponent<RectTransform>();
