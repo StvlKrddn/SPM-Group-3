d5eb5f813a208be0c9bd241d247e2e634a9a8dfb
Markus Koffman <markus.koffman@ownit.nu>
2022-05-24 15:00:23 +0200

    Changes

--- SPM Grupp3/Assets/Scripts/Markus K Test/TankUpgradeTree.cs
+++ SPM Grupp3/Assets/Scripts/Markus K Test/TankUpgradeTree.cs
@@ -25,7 +25,15 @@ public abstract class TankUpgradeTree : MonoBehaviour
         gameManager = FindObjectOfType<GameManager>(); 
 	}
 
-    public abstract void UpgradeOne();
+	private void OnEnable()
+	{
+        if (UpgradeController.currentUpgradeLevel == 3 && abilityReady == false)
+        {
+            StartCoroutine(ResetAbility());
+        }
+	}
+
+	public abstract void UpgradeOne();
 
     public virtual void UpgradeTwo()
     {
@@ -43,7 +51,7 @@ public abstract class TankUpgradeTree : MonoBehaviour
         if (abilityReady == true)
         {
             abilityReady = false;
-   //         StartCoroutine(ResetAbility());
+            StartCoroutine(ResetAbility());
             return true;
         }
 
