d6bd4a7e96902cb7486b4bee890f7030fc7e5354
Markus Koffman <markus.koffman@ownit.nu>
2022-06-04 15:48:30 +0200

    yes

--- SPM Grupp3/Assets/Scripts/UI/SelectionWheel/WheelSelection.cs
+++ SPM Grupp3/Assets/Scripts/UI/SelectionWheel/WheelSelection.cs
@@ -398,8 +398,8 @@ public class WheelSelection : MonoBehaviour
                 p1Penetration.color = Color.green;
                 p2FireRate.color = Color.green;
 
-                tankUpgradeTitleP1.text = "Player 1: Sniper (LVL 1)";
-                tankUpgradeTitleP2.text = "Player 2: Fire (LVL 1)";
+                tankUpgradeTitleP1.text = "Sniper (LVL 1)";
+                tankUpgradeTitleP2.text = "Fire (LVL 1)";
                 break;
             case 1:
                 p1Range.color = Color.green;
@@ -409,19 +409,19 @@ public class WheelSelection : MonoBehaviour
                 p2Damage.color = Color.green;
                 p2FireRate.color = Color.green;
 
-                tankUpgradeTitleP1.text = "Player 1: Sniper (LVL 2)";
-                tankUpgradeTitleP2.text = "Player 2 Fire (LVL 2)";
+                tankUpgradeTitleP1.text = "Sniper (LVL 2)";
+                tankUpgradeTitleP2.text = "Fire (LVL 2)";
                 break;
             case 2:
                 p1Ability.color = Color.green;
                 p2Ability.color = Color.green;
 
-                tankUpgradeTitleP1.text = "Player 1: Sniper (LVL 3)";
-                tankUpgradeTitleP2.text = "Player 2: Fire (LVL 3)";
+                tankUpgradeTitleP1.text = "Sniper (LVL 3)";
+                tankUpgradeTitleP2.text = "Fire (LVL 3)";
                 break;
             case 3:
-                tankUpgradeTitleP1.text = "Player 1: Sniper (LVL Max)";
-                tankUpgradeTitleP2.text = "Player 2: Fire (LVL Max)";
+                tankUpgradeTitleP1.text = "Sniper (LVL Max)";
+                tankUpgradeTitleP2.text = "Fire (LVL Max)";
                 break;
         }
 
